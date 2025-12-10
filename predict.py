4c08cbae9ff1377e786f472412a04ee8ac3a5ac3
tariqcys <tariqcys@hotmail.com>
2025-12-10 21:43:56 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,17 @@
+import joblib
+import json
+import pandas as pd
+import lzma
+import feature_builder
+
+with open("feature_columns.json", "r") as f:
+    feature_cols = json.load(f)
+
+with lzma.open("light_model.pkl.xz", "rb") as f:
+    model = joblib.load(f)
+
 def predict_url(url):
-    features = extract_features(url)
+    features = feature_builder.extract_features(url)
 
     print("RAW FEATURES:", features)
     print("RAW FEATURES SHAPE:", features.shape)
@@ -7,7 +19,6 @@ def predict_url(url):
     X = pd.DataFrame([features], columns=feature_cols)
 
     print("DATAFRAME SHAPE:", X.shape)
-    print("DATAFRAME HEAD:")
     print(X.head())
 
     prediction = model.predict(X)[0]
